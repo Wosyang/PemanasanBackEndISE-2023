f560788e9196df2e898b60409ca047e23bafef54
nerdypenguin <eugeniaindrawan@gmail.com>
2023-04-14 22:13:45 +0700

    Issue #2

---
+++ app/Core/Domain/Repository/SqlKecamatanRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Core\Domain\Repository;
+
+use App\Core\Domain\Models\Kecamatan\Kecamatan;
+use Illuminate\Support\Facades\DB;
+
+class SqlKecamatanRepository
+{
+    public function getAll(): array
+    {
+        $rows = DB::table('kecamatan')->get();
+
+        if (!$rows) {
+            return null;
+        }
+
+        return $this->constructFromRows($rows->all());
+    }
+
+    public function find(int $id): ?Kecamatan
+    {
+        $row = DB::table('kecamatan')->where('id', $id)->first();
+
+        if (!$row) {
+            return null;
+        }
+
+        return $this->constructFromRows([$row])[0];
+    }
+
+    public function constructFromRows(array $rows): array
+    {
+        $kecamatan = [];
+        foreach ($rows as $row) {
+            $provinsi[] = new Kecamatan(
+                $row->id,
+                $row->name,
+            );
+        }
+        return $provinsi;
+    }
+}
